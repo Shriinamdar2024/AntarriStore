e4a2fe9802b24768ae507392b7fce5e0e392a19e
Shriinamdar2024 <shriinamdar88@gmail.com>
2026-03-27 10:50:11 +0530

    fix: removed localhost API

--- antari-backend/index.js
+++ antari-backend/index.js
@@ -11,20 +11,12 @@ const app = express();
 
 // --- MIDDLEWARE ---
 app.use(cors({
-    origin: function (origin, callback) {
-        const allowedOrigins = [
-            "http://localhost:5173",
-            "https://antarristore.vercel.app"
-        ];
-
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("CORS not allowed"));
-        }
-    },
-    credentials: true
+    origin: "https://antarristore.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
